44dcc1f2ba77651d65c508e023b248264540da46
David Jones <davidthomjones@gmail.com>
2016-06-10 13:11:17 +1000

    Got the dhits model/view working from command line

---
+++ Analytics/support/dhits.pl
@@ -0,0 +1,39 @@
+#
+# FILE:     dhits.pl
+# PURPOSE:  Simple test app for Analytics::dhits.pm
+#
+
+BEGIN
+{
+  push @INC, "/Library/Perl/5.8.1";
+}
+
+use strict;
+use Data::Dumper;
+
+use webfuse::lib::QILTers::Analytics::dhits;
+use webfuse::lib::QILTers::Analytics::dhits_View;
+
+
+my $model = QILTers::Analytics::dhits->new( OFFERING => "EDC3100_2015_1" );
+
+#foreach my $set ( qw/ all Online Springfield Toowoomba  / ) {
+#
+#    my $subset = $model->getSubset( $set );
+#    my $all = $model->getSubsetQuantStats( $subset );
+#
+#    next if ( $all->count() == 0 );
+#print "**** SET $set\n";
+#foreach my $field ( qw/ count sum mean min max standard_deviation / ) {
+#    print "-- $field " . $all->$field() . "\n";
+#}
+#}
+
+#print Dumper( $model->{TOTAL_DHITS} );
+
+my $view  = QILTers::Analytics::dhits_View->new( MODEL => $model );
+my $string = $view->Display( 
+                COURSE => "EDC3100", OFFERING => "2015_1",
+                            SUBSET => "all" );
+
+print $string;
